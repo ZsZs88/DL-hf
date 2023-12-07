c0ecbb190e2ae9c78c38248d348d4de8aca51758
ZsZs88 <zsali.zsombor@gmail.com>
2023-12-07 19:30:45 +0100

    sample to streamlit

--- src/streamlit_app/app.py
+++ src/streamlit_app/app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+import train
+import paths
+
 
 def render(celeba_trainer, danbooru_trainer):
     session = st.session_state
@@ -10,18 +13,25 @@ def render(celeba_trainer, danbooru_trainer):
         if "celeba_img" in session:
             st.image(session["celeba_img"])
         if st.button(label="Generate", key="celeba_generate_btn"):
-            # session["celeba_img"] = celeba_trainer.sample()
+            session["celeba_img"] = celeba_trainer.sample_one_for_GUI()
             st.rerun()
     with danbooru_col:
         if "danbooru_img" in session:
             st.image(session["danbooru_img"])
         if st.button(label="Generate", key="danbooru_generate_btn"):
-            # session["danbooru_img"] = danbooru_trainer.sample()
+            session["danbooru_img"] = danbooru_trainer.sample_one_for_GUI()
             st.rerun()
 
 
-
 if __name__ == "__main__":
-    celeba_trainer = None
-    danbooru_trainer = None
+    celeba_trainer = train.Trainer()
+    celeba_trainer.add_paths(paths.celeba)
+    celeba_trainer.add_model(
+        "/home/l0026@njmcs.local/data/Generative/datasets/celeba/models/sample_30.pth"
+    )
+    danbooru_trainer = train.Trainer()
+    danbooru_trainer.add_paths(paths.danbooru)
+    danbooru_trainer.add_model(
+        "/home/l0026@njmcs.local/data/Generative/datasets/danbooru/models/sample_30.pth"
+    )
     render(celeba_trainer=celeba_trainer, danbooru_trainer=danbooru_trainer)
