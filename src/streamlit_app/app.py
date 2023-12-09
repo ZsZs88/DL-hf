1951f4f3cba58129b0772e8e2a7b7381c3143c25
csiklaura <csiklaura2001@gmail.com>
2023-12-09 09:24:17 +0100

    Add subheader to columns

--- src/streamlit_app/app.py
+++ src/streamlit_app/app.py
@@ -4,18 +4,20 @@ import train
 import paths
 
 
-def render(celeba_trainer, danbooru_trainer):
+def render(celeba_trainer: train.Trainer, danbooru_trainer: train.Trainer) -> None:
     session = st.session_state
     st.title(body="Unstable Diffusion")
     celeba_col, danbooru_col = st.columns([1, 1], gap="large")
 
     with celeba_col:
+        st.subheader("CelebA")
         if "celeba_img" in session:
             st.image(session["celeba_img"])
         if st.button(label="Generate", key="celeba_generate_btn"):
             session["celeba_img"] = celeba_trainer.sample_one_for_GUI()
             st.rerun()
     with danbooru_col:
+        st.subheader("Danbooru")
         if "danbooru_img" in session:
             st.image(session["danbooru_img"])
         if st.button(label="Generate", key="danbooru_generate_btn"):
